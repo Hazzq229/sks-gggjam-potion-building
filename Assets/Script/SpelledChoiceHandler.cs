f0dd2219727e25601b7e149fc804a66ab9c18860
Fakih32 <fakihahcmad32@gmail.com>
2025-11-04 15:50:42 +0700

    interaksi bahan dan mantera

--- Assets/Script/SpelledChoiceHandler.cs
+++ Assets/Script/SpelledChoiceHandler.cs
@@ -1,40 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SpelledChoiceHandler : MonoBehaviour
 {
-    private IsiPesananKustomer pesanan;
-    private UangPemain duid;
-    private int bahanindex;
-    public Button[] tombolbahan1;
-    public Button[] tombolbahan2;
-    public Button[] tombolbahan3;
-    
-    public InputField ketikmantra;
-    // Start is called before the first frame update
+    public IsiPesananKustomer pesanan;
+    public UangPemain duid;
+
+    public GameObject[] kelompokBahan;
+
+    private int bahanindex = 0;
+
     void Start()
     {
-        
+
+        for (int i = 0; i < kelompokBahan.Length; i++)
+            kelompokBahan[i].SetActive(i == 0);
+
+
+        for (int group = 0; group < kelompokBahan.Length; group++)
+        {
+            Button[] buttons = kelompokBahan[group].GetComponentsInChildren<Button>();
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                int capturedGroup = group;
+                int capturedIndex = i;
+                buttons[i].onClick.AddListener(() => PilihBahan(capturedGroup, capturedIndex));
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void PilihBahan(int group, int index)
     {
 
-    }
-    public void milihbahan(int indexbahan )
-    {
-        if (bahanindex == pesanan.bahanbenar1)
+        if (group != bahanindex)
+            return;
+
+        int correctIndex = -1;
+
+        switch (bahanindex)
+        {
+            case 0: correctIndex = pesanan.bahanbenar1; break;
+            case 1: correctIndex = pesanan.bahanbenar2; break;
+            case 2: correctIndex = pesanan.bahanbenar3; break;
+        }
+
+
+        if (index == correctIndex)
         {
             duid.tambahuang(100f);
+
         }
         else
         {
             duid.kuranguang(50f);
+
         }
+
+
+        kelompokBahan[bahanindex].SetActive(false);
+
         bahanindex++;
+
+        if (bahanindex < kelompokBahan.Length)
+        {
+            kelompokBahan[bahanindex].SetActive(true);
+        }
+
     }
-    
 }
